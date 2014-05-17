069d591f5318763fcc250c44baf5753aedf71076
HoH <zjulxz@gmail.com>
2014-05-17 19:42:30 -0400

    update palidrome? and count_words

--- ruby-calisthenics/lib/fun_with_strings.rb
+++ ruby-calisthenics/lib/fun_with_strings.rb
@@ -1,9 +1,11 @@
 module FunWithStrings
   def palindrome?
-    self.gsub(/\W/, '') == self.reverse.gsub(/\W/, '')
+    self.downcase.gsub(/\W/, '') == self.reverse.gsub(/\W/, '')
   end
   def count_words
-    # your code here
+    count = Hash.new(0)
+    self.downcase.split.each {|word| count[word.gsub(/\W/,"")] +=1 }
+    return count
   end
   def anagram_groups
     # your code here
