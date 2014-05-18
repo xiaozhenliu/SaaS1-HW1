732520f342e49d4c110a2968f7db617a6285a406
HoH <zjulxz@gmail.com>
2014-05-18 07:13:25 -0700

    Write RPS (HW1-3)

--- ruby-calisthenics/lib/rock_paper_scissors.rb
+++ ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -4,7 +4,18 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    strategy = ["R","P","S"]
+    ps1 = strategy.index(player1[1])
+    ps2 = strategy.index(player2[1])
+
+    raise NoSuchStrategyError.new("Strategy must be one of R,P,S") if (ps1 == nil) or (ps2 == nil)
+    return player1 if ps1 == ps2 
+    
+    case [ps1,ps2]
+    when [1,0],[2,1],[0,2] then return player1
+    when [0,1],[1,2],[2,0] then return player2
+    else "Epic fail"
+    end
   end
 
   def self.tournament_winner(tournament)
